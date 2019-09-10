861ffb08745984cc39931b8bfcf99699ad2d335b
Dima <dima.dibb1@gmail.com>
2019-09-09 23:05:05 -0700

    finish game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,20 @@ var lose = 0;
 var keyWord;
 var str;
 var attempts;
+var letters;
+
 
 var userLoseH3 = document.getElementById("userLose");
 var userWinH3 = document.getElementById("userWin");
 var attemptsSpan = document.getElementById("getAttempts");
 var getWordSpan = document.getElementById("getWord");
 var gamePictures = document.getElementById("gamePicture");
+var lettersUsedSpan = document.getElementById("lettersUsed");
 
 
 function startGame() {
     attempts = 8;
+    letters = "";
     keyWord = dogsBreeds[Math.floor(Math.random() * dogsBreeds.length)];
     str = "";
     for (var i = 0; i < keyWord.length; i++) {
@@ -30,6 +34,7 @@ function updateUI() {
     getWordSpan.innerHTML = str;
     userLoseH3.innerHTML = lose;
     userWinH3.innerHTML = win;
+    lettersUsedSpan.innerHTML = letters;
 }
 startGame();
 updateUI();
@@ -41,18 +46,19 @@ document.onkeyup = function (event) {
             str = str.slice(0, i) + c + str.slice(i + 1, str.length);
         }
     }
-    if (keyWord.indexOf(c) === -1) {
+    if (keyWord.indexOf(c) === -1 && letters.indexOf(c) === -1) {
+        letters += c;
         attempts--;
     }
     if (str === keyWord) {
         win++;
-        gamePictures.setAttribute("src","assets/images/"+ keyWord + ".jpg");
+        gamePictures.setAttribute("src", "assets/images/" + keyWord + ".jpg");
         startGame();
     }
     if (attempts === 0) {
         lose++;
         startGame();
-       
+
     }
 
     updateUI();
